886e686f6d1749b9ff8dfd895fe91030093dd1ed
ismayilovcode <ismayilovterlan508@gmail.com>
2024-03-06 21:40:24 +0400

    added project

---
+++ index..js
@@ -0,0 +1,23 @@
+// version 1
+
+// let x = +prompt("x:");
+
+// let a = x % 10;
+// x = (x - a) / 10;
+// let b = x % 10;
+// x = (x - b) / 10;
+// let c = x % 10;
+// x = (x - c) / 10;
+
+// alert(a + b + c);
+
+// version 2
+
+// let y = 0;
+
+// while (x > 0) {
+//   y += x % 10;
+//   x = (x - (x % 10)) / 10;
+// }
+
+// alert(y);
